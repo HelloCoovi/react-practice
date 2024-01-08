94724cd5ca9487a091114c699b981b951dafaf85
jangg0627 <jangg0627@naver.com>
2024-01-08 19:37:42 +0900

    🎯 FetchAPI 실습 - jsonplaceholder에서 데이터 받아옴 - useEffect로 반복적인 API 호출을 막음

--- src/components/apiCallMethod/FetchAPI.jsx
+++ src/components/apiCallMethod/FetchAPI.jsx
@@ -1,16 +1,42 @@
+import { useState, useEffect } from "react";
+
 import ApiDataRenderer from "../ApiDataRenderer.jsx";
 
 export default function FetchAPI() {
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+
+      setUserData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <ApiDataRenderer
-        data={[
-          { username: "Nick", name: "jabs", email: "usermail@gmail.com", phone: "010-0000-0000" },
-        ]}
-      />
+      <ApiDataRenderer data={userData} />
       <pre style={{ background: "#eee", padding: "10px" }}>
-        <code>{"codeString"}</code>
+        <code>{sourceCode}</code>
       </pre>
     </div>
   );
 }
+
+const sourceCode = `export default function FetchAPI() {
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+
+      setUserData(data);
+    };
+
+    fetchData();
+  }, []);
+}`;
